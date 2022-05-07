dda3f7691397f27efd0201b1531fddeef8d3a43b
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-05-07 08:27:39 +0700

    Add intro , intro1, intro2

---
+++ src/app/intro/intro-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IntroPage } from './intro.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: IntroPage
+  },
+  {
+    path: 'intro1',
+    loadChildren: () => import('./intro1/intro1.module').then( m => m.Intro1PageModule)
+  },
+  {
+    path: 'intro2',
+    loadChildren: () => import('./intro2/intro2.module').then( m => m.Intro2PageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class IntroPageRoutingModule {}
